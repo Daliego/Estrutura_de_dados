6b38184eb60633e81738e9acf253445b2f8c85f1
Diego <102397299+Daliego@users.noreply.github.com>
2022-10-04 11:33:03 -0300

    Add files via upload

---
+++ atividade_de_pilha/atividade_cap_04/4.4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "fila.hpp"
+#define tempo 3
+
+using namespace std;
+
+int main(){
+    Fila* F = new Fila();
+    F->inserir_na_fila(17);
+    F->inserir_na_fila(25);
+    F->inserir_na_fila(39);
+    F->inserir_na_fila(46);
+
+    while(F->vazia() == false){
+        int x = F->retirar_da_fila();
+        int p = x/10;
+        int t = x%10;
+
+        if(t > 3){
+            F->inserir_na_fila(p*10+(t-tempo));
+        }else{
+            cout << x << "O Processo foi concluido" << endl;
+        }
+    }
+    
+    //A ordem de conclusão dos processos foi 17, depois 25, em segida, 39, e depois 46;
+
+}
